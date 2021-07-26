9910e4fd635706e7180d8fb0f6f54ec1c2b4749f
FizeChan <chenfengzhan@qq.com>
2021-07-26 17:02:46 +0800

    Http修改及单元测试。

--- tests/TestHttp.php
+++ tests/TestHttp.php
@@ -11,88 +11,9 @@ class TestHttp extends TestCase
         $headers = [
             'accept' => 'application/json'
         ];
-        $content = Http::get('https://httpbin.org/get', $headers);
+        $response = Http::get('https://httpbin.org/get', $headers);
+        $content = $response->getBody()->getContents();
         var_dump($content);
         self::assertIsString($content);
-    }
-
-    public function testPost()
-    {
-        $headers = [
-            'Content-Type' => 'application/x-www-form-urlencoded'
-        ];
-        $body = [
-            'q' => '定制化翻译API语言方向目前只支持中文和英文。',
-            'from' => 'zh',
-            'to' => 'en',
-            'appid' => '20160118000009064',
-            'salt' => '123456',
-            'sign' => '9ac0dad8ab7abafc710bf5a9a8516e51'
-        ];
-        $content = Http::post('http://api.fanyi.baidu.com/api/trans/vip/translate', $body, $headers);
-        echo $content;
-        self::assertNotEmpty($content);
-    }
-
-    public function testOptions()
-    {
-        $headers = [
-            'accept' => 'application/json'
-        ];
-        $content = Http::options('https://httpbin.org/options', $headers);
-        echo $content;
-        self::assertNotEmpty($content);
-    }
-
-    public function testDelete()
-    {
-
-    }
-
-
-
-    public function testWrapped()
-    {
-
-    }
-
-    public function testUnlink()
-    {
-
-    }
-
-    public function testTrace()
-    {
-
-    }
-
-    public function testLink()
-    {
-
-    }
-
-    public function testMove()
-    {
-
-    }
-
-    public function testCopy()
-    {
-
-    }
-
-    public function testHead()
-    {
-
-    }
-
-    public function testPatch()
-    {
-
-    }
-
-    public function testPut()
-    {
-
     }
 }
