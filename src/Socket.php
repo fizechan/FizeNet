9368764b6c669c78b48dd450d51a6979f715d2b1
FizeChan <chenfengzhan@qq.com>
2021-07-29 15:13:19 +0800

    Socket修改及单元测试。

--- src/Socket.php
+++ src/Socket.php
@@ -6,7 +6,6 @@ use Exception;
 
 /**
  * Socket 套接字
- * @todo 待测试
  */
 class Socket
 {
@@ -14,7 +13,7 @@ class Socket
     /**
      * @var resource 当前套接字
      */
-    private $_socket;
+    private $socket;
 
     /**
      * 构造函数
@@ -22,7 +21,7 @@ class Socket
      */
     public function __construct($socket = null)
     {
-        $this->_socket = $socket;
+        $this->socket = $socket;
     }
 
     /**
@@ -39,9 +38,9 @@ class Socket
      * @return Socket
      * @throws Exception
      */
-    public function accept()
+    public function accept(): Socket
     {
-        $socket = socket_accept($this->_socket);
+        $socket = socket_accept($this->socket);
         if ($socket === false) {
             throw new Exception(self::strerror($this->lastError()), $this->lastError());
         }
@@ -57,9 +56,9 @@ class Socket
      * @param int    $port    指定端口
      * @return bool
      */
-    public function bind($address, $port = 0)
+    public function bind(string $address, int $port = 0): bool
     {
-        return socket_bind($this->_socket, $address, $port);
+        return socket_bind($this->socket, $address, $port);
     }
 
     /**
@@ -67,7 +66,7 @@ class Socket
      */
     public function clearError()
     {
-        socket_clear_error($this->_socket);
+        socket_clear_error($this->socket);
     }
 
     /**
@@ -75,20 +74,18 @@ class Socket
      */
     public function close()
     {
-        if ($this->_socket !== null) {
-            socket_close($this->_socket);
-        }
+        socket_close($this->socket);
     }
 
     /**
      * 计算应分配用于接收辅助数据的缓冲区的大小。
      *
-     * 该方法由于官方文档不齐全，暂不建议使用
+     * @notice 该方法由于官方文档不齐全，暂不建议使用
      * @param int $level
      * @param int $type
      * @return int
      */
-    public static function cmsgSpace($level, $type)
+    public static function cmsgSpace(int $level, int $type): int
     {
         return socket_cmsg_space($level, $type);
     }
@@ -99,9 +96,9 @@ class Socket
      * @param int    $port    端口号
      * @return bool 成功时返回 TRUE ， 或者在失败时返回 FALSE
      */
-    public function connect($address, $port = 0)
+    public function connect(string $address, int $port = 0): bool
     {
-        return socket_connect($this->_socket, $address, $port);
+        return socket_connect($this->socket, $address, $port);
     }
 
     /**
@@ -111,13 +108,13 @@ class Socket
      * @return Socket
      * @throws Exception
      */
-    public function createListen($port, $backlog = 128)
+    public function createListen(int $port, int $backlog = 128): Socket
     {
         $socket = socket_create_listen($port, $backlog);
         if ($socket === false) {
             throw new Exception(self::strerror($this->lastError()), $this->lastError());
         }
-        $this->_socket = $socket;
+        $this->socket = $socket;
         return $this;
     }
 
@@ -132,7 +129,7 @@ class Socket
      * @param array $obj      注意该数组保存的是Socket对象而非socket资源
      * @return bool 成功时返回 TRUE ， 或者在失败时返回 FALSE 。
      */
-    public static function createPair($domain, $type, $protocol, array &$obj)
+    public static function createPair(int $domain, int $type, int $protocol, array &$obj): bool
     {
         $fd = [];
         $rst = socket_create_pair($domain, $type, $protocol, $fd);
@@ -155,13 +152,13 @@ class Socket
      * @return Socket
      * @throws Exception
      */
-    public function create($domain, $type, $protocol)
+    public function create(int $domain, int $type, int $protocol): Socket
     {
         $socket = socket_create($domain, $type, $protocol);
         if ($socket === false) {
             throw new Exception(self::strerror($this->lastError()), $this->lastError());
         }
-        $this->_socket = $socket;
+        $this->socket = $socket;
         return $this;
     }
 
@@ -169,45 +166,46 @@ class Socket
      * 获取当前套接字的套接字选项
      * @param int $level   指定协议级别
      * @param int $optname 选项名
-     * @return mixed
+     * @return array|int|false
      */
-    public function getOption($level, $optname)
+    public function getOption(int $level, int $optname)
     {
-        return socket_get_option($this->_socket, $level, $optname);
+        return socket_get_option($this->socket, $level, $optname);
     }
 
     /**
      * 获取当前套接字的套接字选项
+     *
      * 使用socket_get_option()的别名socket_getopt()
      * @param int $level   指定协议级别
      * @param int $optname 选项名
-     * @return mixed
+     * @return array|int|false
      */
-    public function getOpt($level, $optname)
+    public function getOpt(int $level, int $optname)
     {
-        return socket_getopt($this->_socket, $level, $optname);
+        return socket_getopt($this->socket, $level, $optname);
     }
 
     /**
-     * 获取主机/端口或UNIX文件系统路径的远端
-     * @param string $address 获取到的主机
-     * @param int    $port    获取到的端口
+     * 获取远端主机/端口或UNIX文件系统路径
+     * @param string   $address 获取到的主机
+     * @param int|null $port    获取到的端口
      * @return bool 成功时返回 TRUE ， 或者在失败时返回 FALSE
      */
-    public function getPeerName(&$address, &$port = null)
+    public function getPeerName(string &$address, int &$port = null): bool
     {
-        return socket_getpeername($this->_socket, $address, $port);
+        return socket_getpeername($this->socket, $address, $port);
     }
 
     /**
-     * 获取具体连接的主机/端口或UNIX文件系统路径的远端
-     * @param string $addr 获取到的主机
-     * @param int    $port 获取到的端口
+     * 获取本地具体连接的主机/端口或UNIX文件系统路径
+     * @param string   $addr 获取到的主机
+     * @param int|null $port 获取到的端口
      * @return bool 成功时返回 TRUE ， 或者在失败时返回 FALSE
      */
-    public function getSockName(&$addr, &$port = null)
+    public function getSockName(string &$addr, int &$port = null): bool
     {
-        return socket_getsockname($this->_socket, $addr, $port);
+        return socket_getsockname($this->socket, $addr, $port);
     }
 
     /**
@@ -216,13 +214,13 @@ class Socket
      * @return Socket
      * @throws Exception
      */
-    public function importStream($stream)
+    public function importStream($stream): Socket
     {
         $socket = socket_import_stream($stream);
         if ($socket == false) {
             throw new Exception(self::strerror($this->lastError()), $this->lastError());
         }
-        $this->_socket = $socket;
+        $this->socket = $socket;
         return $this;
     }
 
@@ -230,9 +228,9 @@ class Socket
      * 获取最后的错误代码
      * @return int
      */
-    public function lastError()
+    public function lastError(): int
     {
-        return socket_last_error($this->_socket);
+        return socket_last_error($this->socket);
     }
 
     /**
@@ -240,9 +238,9 @@ class Socket
      * @param int $backlog 连接队列的最大长度
      * @return bool 成功时返回 TRUE ， 或者在失败时返回 FALSE
      */
-    public function listen($backlog = 0)
+    public function listen(int $backlog = 0): bool
     {
-        return socket_listen($this->_socket, $backlog);
+        return socket_listen($this->socket, $backlog);
     }
 
     /**
@@ -251,9 +249,9 @@ class Socket
      * @param int $type   读取、断行方式
      * @return string
      */
-    public function read($length, $type = PHP_BINARY_READ)
+    public function read(int $length, int $type = PHP_BINARY_READ): string
     {
-        return socket_read($this->_socket, $length, $type);
+        return socket_read($this->socket, $length, $type);
     }
 
     /**
@@ -270,9 +268,9 @@ class Socket
      * @param int    $flags 标识
      * @return int 返回一个数字，表示接收到的字节数。如果发生了错误，则返回 FALSE
      */
-    public function recv(&$buf, $len, $flags)
+    public function recv(string &$buf, int $len, int $flags): int
     {
-        return socket_recv($this->_socket, $buf, $len, $flags);
+        return socket_recv($this->socket, $buf, $len, $flags);
     }
 
     /**
@@ -284,16 +282,16 @@ class Socket
      *   flags的值可以为下列任意flag的组合。
      *   使用按位或运算符(|)来 组合不同的flag。
      *   MSG_OOB/MSG_PEEK/MSG_WAITALL/MSG_DONTWAIT
-     * @param string $buf   从socket中获取的数据将被保存在由 buf 制定的变量中。
-     * @param int    $len   长度最多为 len 字节的数据将被接收。
-     * @param int    $flags 标识
-     * @param string $name  获取到的主机
-     * @param int    $port  获取到的端口
+     * @param string   $buf   从socket中获取的数据将被保存在由 buf 制定的变量中。
+     * @param int      $len   长度最多为 len 字节的数据将被接收。
+     * @param int      $flags 标识
+     * @param string   $name  获取到的主机
+     * @param int|null $port  获取到的端口
      * @return int 返回一个数字，表示接收到的字节数。如果发生了错误，则返回 FALSE
      */
-    public function recvFrom(&$buf, $len, $flags, &$name, &$port = null)
+    public function recvFrom(string &$buf, int $len, int $flags, string &$name, int &$port = null): int
     {
-        return socket_recvfrom($this->_socket, $buf, $len, $flags, $name, $port);
+        return socket_recvfrom($this->socket, $buf, $len, $flags, $name, $port);
     }
 
     /**
@@ -304,27 +302,27 @@ class Socket
      *   flags的值可以为下列任意flag的组合。
      *   使用按位或运算符(|)来 组合不同的flag。
      *   MSG_OOB/MSG_PEEK/MSG_WAITALL/MSG_DONTWAIT
-     * @param string $message
-     * @param int    $flags 标识
+     * @param array    $message 读取到的信息
+     * @param int|null $flags 标识
      * @return int 返回一个数字，表示接收到的字节数。如果发生了错误，则返回 FALSE
      */
-    public function recvMsg($message, $flags = null)
+    public function recvMsg(array &$message, int $flags = null): int
     {
-        return socket_recvmsg($this->_socket, $message, $flags);
+        return socket_recvmsg($this->socket, $message, $flags);
     }
 
     /**
      * socket多路选择
      *
      * 注意返回的是socket资源而非Socket对象
-     * @param array $read    监听到的发生读取的socket资源
-     * @param array $write   监听到的发生写入的socket资源
-     * @param array $except  监听到的发生异常的socket资源
-     * @param int   $tv_sec  服务端超时时间
-     * @param int   $tv_usec 客服端超时时间
+     * @param array|null $read    监听到的发生读取的socket资源
+     * @param array|null $write   监听到的发生写入的socket资源
+     * @param array|null $except  监听到的发生异常的socket资源
+     * @param int        $tv_sec  服务端超时时间
+     * @param int        $tv_usec 客服端超时时间
      * @return int 返回获取到的socket个数
      */
-    public static function select(array &$read, array &$write, array &$except, $tv_sec, $tv_usec = 0)
+    public static function select(?array &$read, ?array &$write, ?array &$except, int $tv_sec, int $tv_usec = 0): int
     {
         return socket_select($read, $write, $except, $tv_sec, $tv_usec);
     }
@@ -336,9 +334,9 @@ class Socket
      * @param int    $flags MSG_OOB/MSG_EOR/MSG_EOF/MSG_DONTROUTE
      * @return int 成功时返回发送的字节数量，失败时返回false
      */
-    public function send($buf, $len, $flags)
+    public function send(string $buf, int $len, int $flags): int
     {
-        return socket_send($this->_socket, $buf, $len, $flags);
+        return socket_send($this->socket, $buf, $len, $flags);
     }
 
     /**
@@ -349,9 +347,9 @@ class Socket
      * @param int   $flags
      * @return int 成功时返回发送的字节数量，失败时返回false
      */
-    public function sendMsg(array $message, $flags)
+    public function sendmsg(array $message, int $flags): int
     {
-        return socket_sendmsg($this->_socket, $message, $flags);
+        return socket_sendmsg($this->socket, $message, $flags);
     }
 
     /**
@@ -363,27 +361,27 @@ class Socket
      * @param int    $port  获取到的端口
      * @return int 成功时返回发送的字节数量，失败时返回false
      */
-    public function sendTo($buf, $len, $flags, $addr, $port = 0)
+    public function sendto(string $buf, int $len, int $flags, string $addr, int $port = 0): int
     {
-        return socket_sendto($this->_socket, $buf, $len, $flags, $addr, $port);
+        return socket_sendto($this->socket, $buf, $len, $flags, $addr, $port);
     }
 
     /**
      * 设置为块模式
      * @return bool
      */
-    public function setBlock()
+    public function setBlock(): bool
     {
-        return socket_set_block($this->_socket);
+        return socket_set_block($this->socket);
     }
 
     /**
      * 设置为非块模式
      * @return bool
      */
-    public function setNonblock()
+    public function setNonblock(): bool
     {
-        return socket_set_nonblock($this->_socket);
+        return socket_set_nonblock($this->socket);
     }
 
     /**
@@ -393,9 +391,9 @@ class Socket
      * @param mixed $optval  选项值
      * @return bool 成功时返回 TRUE ， 或者在失败时返回 FALSE
      */
-    public function setOption($level, $optname, $optval)
+    public function setOption(int $level, int $optname, $optval): bool
     {
-        return socket_set_option($this->_socket, $level, $optname, $optval);
+        return socket_set_option($this->socket, $level, $optname, $optval);
     }
 
     /**
@@ -407,9 +405,9 @@ class Socket
      * @param mixed $optval  选项值
      * @return bool 成功时返回 TRUE ， 或者在失败时返回 FALSE
      */
-    public function setOpt($level, $optname, $optval)
+    public function setOpt(int $level, int $optname, $optval): bool
     {
-        return socket_setopt($this->_socket, $level, $optname, $optval);
+        return socket_setopt($this->socket, $level, $optname, $optval);
     }
 
     /**
@@ -417,9 +415,9 @@ class Socket
      * @param int $how 0关闭读取、1关闭写入、2关闭全部
      * @return bool
      */
-    public function shutdown($how = 2)
+    public function shutdown(int $how = 2): bool
     {
-        return socket_shutdown($this->_socket, $how);
+        return socket_shutdown($this->socket, $how);
     }
 
     /**
@@ -427,7 +425,7 @@ class Socket
      * @param int $errno socket错误代码
      * @return string
      */
-    public static function strerror($errno)
+    public static function strerror(int $errno): string
     {
         return socket_strerror($errno);
     }
@@ -438,8 +436,8 @@ class Socket
      * @param int    $length 指定要写入的最大字节长度
      * @return int 返回写入的字节长度
      */
-    public function write($buffer, $length = 0)
+    public function write(string $buffer, int $length = 0): int
     {
-        return socket_write($this->_socket, $buffer, $length);
+        return socket_write($this->socket, $buffer, $length);
     }
 }
---
+++ tests/TestSocket.php
@@ -0,0 +1,471 @@
+<?php
+
+
+use fize\net\Socket;
+use PHPUnit\Framework\TestCase;
+
+class TestSocket extends TestCase
+{
+
+    public function testAccept()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $ret = $sock->bind($ip, $port);
+        $ret = $sock->listen(4);
+
+        $count = 0;
+        do {
+            $msgsock = $sock->accept();
+            var_dump($msgsock);
+            self::assertInstanceOf(Socket::class, $msgsock);
+            $msg ="测试成功！\n";
+            $msgsock->write($msg, strlen($msg));
+
+            $buf = $msgsock->read(8192);
+            $talkback = "收到的信息:$buf\n";
+            echo $talkback;
+            $msgsock->close();
+            if(++$count >= 1){
+                break;
+            }
+        } while (true);
+        $sock->close();
+    }
+
+    public function testBind()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $ret = $sock->bind($ip, $port);
+        self::assertTrue($ret);
+        $ret = $sock->listen(4);
+
+        $count = 0;
+        do {
+            $msgsock = $sock->accept();
+            $msg ="测试成功！\n";
+            $msgsock->write($msg, strlen($msg));
+
+            $buf = $msgsock->read(8192);
+            $talkback = "收到的信息:$buf\n";
+            echo $talkback;
+            $msgsock->close();
+            if(++$count >= 1){
+                break;
+            }
+        } while (true);
+        $sock->close();
+    }
+
+    public function testClearError()
+    {
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $sock->clearError();
+        self::assertInstanceOf(Socket::class, $sock);
+    }
+
+    public function testClose()
+    {
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $sock->close();
+        self::assertInstanceOf(Socket::class, $sock);
+    }
+
+    public function testCmsgSpace()
+    {
+        $rst = Socket::cmsgSpace(SOL_SOCKET, SO_TYPE);
+        var_dump($rst);
+        self::assertIsInt($rst);
+    }
+
+    public function testConnect()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->connect($ip, $port);
+        self::assertTrue($rst);
+
+        $in = "Ho\r\n";
+        $in .= "first blood\r\n";
+        $out = '';
+        $socket->write($in, strlen($in));
+        while($out = $socket->read(8192)) {
+            echo "接收服务器回传信息成功！\n";
+            echo "接受的内容为:",$out;
+        }
+        echo "关闭SOCKET...\n";
+        $socket->close();
+        echo "关闭OK\n";
+    }
+
+    public function testCreateListen()
+    {
+        $socket = new Socket();
+        $socket2 = $socket->createListen(999);
+        self::assertInstanceOf(Socket::class, $socket);
+        self::assertInstanceOf(Socket::class, $socket2);
+    }
+
+    public function testCreatePair()
+    {
+        $sockets = [];
+        $domain = (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN' ? AF_INET : AF_UNIX);
+        Socket::createPair($domain, SOCK_STREAM, 0, $sockets);
+        var_dump($sockets);
+        self::assertIsArray($sockets);
+    }
+
+    public function testCreate()
+    {
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        self::assertInstanceOf(Socket::class, $sock);
+    }
+
+    public function testGetOption()
+    {
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $value = $sock->getOption(SOL_SOCKET, SO_LINGER);
+        var_dump($value);
+        self::assertNotNull($value);
+    }
+
+    public function testGetOpt()
+    {
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $value = $sock->getOpt(SOL_SOCKET, SO_LINGER);
+        var_dump($value);
+        self::assertNotNull($value);
+    }
+
+    public function testGetPeerName()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $socket->connect($ip, $port);
+        $socket->getPeerName($address, $port);
+        var_dump($address);
+        self::assertNotNull($address);
+        var_dump($port);
+        self::assertNotNull($port);
+        $socket->close();
+    }
+
+    public function testGetSockName()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $socket->connect($ip, $port);
+        $socket->getSockName($address, $port);
+        var_dump($address);
+        self::assertNotNull($address);
+        var_dump($port);
+        self::assertNotNull($port);
+        $socket->close();
+    }
+
+    public function testImportStream()
+    {
+        $socket = new Socket();
+        $stream = stream_socket_server("udp://0.0.0.0:58380", $errno, $errstr, STREAM_SERVER_BIND);
+        $socket->importStream($stream);
+        self::assertInstanceOf(Socket::class, $socket);
+    }
+
+    public function testLastError()
+    {
+        $socket = new Socket();
+        $le = $socket->lastError();
+        var_dump($le);
+        self::assertIsInt($le);
+    }
+
+    public function testListen()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $sock->bind($ip, $port);
+        $ret = $sock->listen(4);
+        self::assertTrue($ret);
+        $sock->close();
+    }
+
+    public function testRead()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->connect($ip, $port);
+        self::assertTrue($rst);
+
+        $in = "Ho\r\n";
+        $in .= "first blood\r\n";
+        $socket->write($in, strlen($in));
+        while($out = $socket->read(8192)) {
+            echo "接收服务器回传信息成功！\n";
+            echo "接受的内容为:",$out;
+        }
+        echo "关闭SOCKET...\n";
+        $socket->close();
+        echo "关闭OK\n";
+    }
+
+    public function testRecv()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->connect($ip, $port);
+        self::assertTrue($rst);
+
+        $in = "Ho\r\n";
+        $in .= "first blood\r\n";
+        $socket->write($in, strlen($in));
+
+        $buf = '';
+        $bytes = $socket->recv($buf, 2048, MSG_WAITALL);
+        echo "Read $bytes bytes from socket_recv(). Closing socket...";
+        echo "接受的内容为:", $buf;
+        echo "关闭SOCKET...\n";
+        $socket->close();
+        echo "关闭OK\n";
+    }
+
+    public function testRecvFrom()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->connect($ip, $port);
+        self::assertTrue($rst);
+
+        $in = "Ho\r\n";
+        $in .= "first blood\r\n";
+        $socket->write($in, strlen($in));
+
+        $buf = '';
+        $iip = '';
+        $iport = null;
+        $bytes = $socket->recvFrom($buf, 2048, MSG_WAITALL, $iip, $iport);
+        echo "Read $bytes bytes from socket_recv(). Closing socket...";
+        echo "接受的内容为:", $buf;
+        echo "关闭SOCKET...\n";
+        $socket->close();
+        echo "关闭OK\n";
+        var_dump($iip);
+        var_dump($iport);
+    }
+
+    public function testRecvMsg()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->connect($ip, $port);
+        self::assertTrue($rst);
+
+        $in = "Ho\r\n";
+        $in .= "first blood\r\n";
+        $socket->write($in, strlen($in));
+
+        $message = ['controllen' => 2048];
+        $bytes = $socket->recvMsg($message, MSG_WAITALL);
+        echo "Read $bytes bytes from recvMsg Closing socket...";
+        $socket->close();
+        var_dump($message);
+        self::assertIsArray($message);
+    }
+
+    public function testSelect()
+    {
+        $r = [];
+        $w = [];
+        $e = null;
+        Socket::select($r, $w, $e, 0);
+        var_dump($r);
+        var_dump($w);
+        self::assertIsArray($r);
+        self::assertIsArray($w);
+    }
+
+    public function testSend()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->connect($ip, $port);
+        self::assertTrue($rst);
+
+        $in = "Ho\r\n";
+        $in .= "first blood\r\n";
+        $socket->send($in, strlen($in), MSG_DONTROUTE);
+        while($out = $socket->read(8192)) {
+            echo "接收服务器回传信息成功！\n";
+            echo "接受的内容为:",$out;
+        }
+        echo "关闭SOCKET...\n";
+        $socket->close();
+        echo "关闭OK\n";
+    }
+
+    public function testSendmsg()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->connect($ip, $port);
+        self::assertTrue($rst);
+        $message = [
+            "Ho",
+            "first blood"
+        ];
+        $bytes = $socket->sendmsg($message, 0);
+        var_dump($bytes);
+        self::assertIsInt($bytes);
+    }
+
+    public function testSendto()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $socket->connect($ip, $port);
+
+        $in = "Ho\r\n";
+        $in .= "first blood\r\n";
+        $bytes = $socket->sendto($in, strlen($in), MSG_DONTROUTE, $ip, $port);
+        var_dump($bytes);
+        self::assertIsInt($bytes);
+        while($out = $socket->read(8192)) {
+            echo "接收服务器回传信息成功！\n";
+            echo "接受的内容为:",$out;
+        }
+        echo "关闭SOCKET...\n";
+        $socket->close();
+        echo "关闭OK\n";
+    }
+
+    public function testSetBlock()
+    {
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->setBlock();
+        self::assertTrue($rst);
+    }
+
+    public function testSetNonblock()
+    {
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->setNonblock();
+        self::assertTrue($rst);
+    }
+
+    public function testSetOption()
+    {
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->setOption(SOL_SOCKET, SO_REUSEADDR, 1);
+        self::assertTrue($rst);
+    }
+
+    public function testSetOpt()
+    {
+        $socket = new Socket();
+        $socket->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $rst = $socket->setOpt(SOL_SOCKET, SO_REUSEADDR, 1);
+        self::assertTrue($rst);
+    }
+
+    public function testShutdown()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $ret = $sock->bind($ip, $port);
+        $ret = $sock->listen(4);
+
+        $count = 0;
+        do {
+            $msgsock = $sock->accept();
+            var_dump($msgsock);
+            self::assertInstanceOf(Socket::class, $msgsock);
+            $msg ="测试成功！\n";
+            $msgsock->write($msg, strlen($msg));
+
+            $buf = $msgsock->read(8192);
+            $talkback = "收到的信息:$buf\n";
+            echo $talkback;
+            $rst = $msgsock->shutdown();
+            self::assertTrue($rst);
+            if(++$count >= 1){
+                break;
+            }
+        } while (true);
+        $sock->close();
+    }
+
+    public function testStrerror()
+    {
+        $strerr = Socket::strerror(SOCKET_NO_DATA);
+        var_dump($strerr);
+        self::assertIsString($strerr);
+    }
+
+    public function testWrite()
+    {
+        $ip = '127.0.0.1';
+        $port = 1935;
+
+        $sock = new Socket();
+        $sock->create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $ret = $sock->bind($ip, $port);
+        $ret = $sock->listen(4);
+
+        $count = 0;
+        do {
+            $msgsock = $sock->accept();
+            var_dump($msgsock);
+            self::assertInstanceOf(Socket::class, $msgsock);
+            $msg ="测试成功！\n";
+            $msgsock->write($msg, strlen($msg));
+
+            $buf = $msgsock->read(8192);
+            $talkback = "收到的信息:$buf\n";
+            echo $talkback;
+            $msgsock->close();
+            if(++$count >= 1){
+                break;
+            }
+        } while (true);
+        $sock->close();
+    }
+}
