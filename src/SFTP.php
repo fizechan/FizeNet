f78cd58f91ae1c9e67da58a7f12ebe5a518a37b8
Fize <chenfengzhan@qq.com>
2021-12-24 11:35:20 +0800

    新增SFTP、SSH2支持。

---
+++ src/SFTP.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fize\Net;
+
+use phpseclib3\Net\SFTP as Common;
+
+/**
+ * SFTP
+ */
+class SFTP extends Common
+{
+
+}
---
+++ src/SSH2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fize\Net;
+
+use phpseclib3\Net\SSH2 as Common;
+
+/**
+ * SSH2
+ */
+class SSH2 extends Common
+{
+
+}
